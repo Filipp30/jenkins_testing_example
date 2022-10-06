2020abf08269a0d4d40aa1fe3af7002c01a63479
Filipp <67765182+Filipp30@users.noreply.github.com>
2022-10-06 10:02:23 +0200

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,1 +1,20 @@
-echo 'test'
+pipeline {
+    agent any
+    stages{
+        stage('Git clone'){
+            steps{
+                git branch: 'main', url: 'https://github.com/Filipp30/jenkins_testing_example.git'
+            }
+        }
+        stage('Test stage'){
+            steps{
+                echo "Testet completed"
+            }
+        }
+        stage('Test build'){
+            steps{
+                echo "Build test completed"
+            }
+        }
+    }
+}
